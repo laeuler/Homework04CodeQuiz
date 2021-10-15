37a0af5ee929cfdab5922b9387973cde67b9eae8
Lars Euler <eulerlars@gmail.com>
2021-10-15 17:57:02 +0200

    Quiz Questions added

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+//Array with Quiz Questions Objects
+
+const quizQuestions = [
+  {
+    //Question 1
+    question: "What does HTML stand for?",
+    a: "Hard Text Markup Language",
+    b: "Hyper Text Markup Location",
+    c: "Hyper Text Markup Language",
+    d: "Hyper Text Meme Language",
+    right: "c",
+  },
+  {
+    //Question 2
+    question: "How do you select all HTML elements on the page",
+    a: "#all",
+    b: ".all",
+    c: "all",
+    d: "*",
+    right: "d",
+  },
+  {
+    //Question 3
+    question: "Which option doesn't let you declare a variable in JavaScript?",
+    a: "var",
+    b: "let",
+    c: "const",
+    d: "new",
+    right: "d",
+  },
+  {
+    //Question 4
+    question: "DOM stands for",
+    a: "Document Object Master",
+    b: "Document Object Model",
+    c: "Data Object Model",
+    d: "Document Order Model",
+    right: "b",
+  },
+  {
+    //Question 5
+    question: "Which one is no semantic element in HTML",
+    a: "<figure>",
+    b: "<mark>",
+    c: "<summary>",
+    d: "navbar",
+    right: "d",
+  },
+];
