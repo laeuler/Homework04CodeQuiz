dd562c17b2fc53075834dea43d90c6a95a78147c
Lars Euler <eulerlars@gmail.com>
2021-10-15 18:28:40 +0200

    timer functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+//assign elements
+var timerEL = document.getElementById("timeleft");
+var startEL = document.getElementById("startnext");
+var minusEL = document.getElementById("testminus");
+
 //Array with Quiz Questions Objects
 
 const quizQuestions = [
@@ -47,3 +52,33 @@ const quizQuestions = [
     right: "d",
   },
 ];
+var timeLeft = 75;
+
+//Timer on the page
+function countdown() {
+  console.log("funciton executed");
+  timeLeft = 75;
+
+  //function to be called every 1000ms
+  var timeInterval = setInterval(function () {
+    //check if time left > 0
+    if (timeLeft > 1) {
+      console.log(timeLeft);
+      timerEL.textContent = "Time left: " + timeLeft + "s";
+      timeLeft--;
+    } else {
+      timerEL.textContent = "Time's up!";
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+}
+
+//function to deduct minus 10 secons
+function minusten() {
+  timeLeft = timeLeft - 10;
+  console.log(timeLeft);
+}
+
+//add event handler
+startEL.addEventListener("click", countdown);
+minusEL.addEventListener("click", minusten);
