95361b53e58512887db7b1de00d9b82ff73b8ed9
Lars Euler <eulerlars@gmail.com>
2021-10-17 13:58:26 +0200

    clean up of code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//Assignment of misc. elements
+//============================================== Assignment of misc. elements ==============================================
 var timerEL = document.getElementById("timeleft");
 var highEL = document.getElementById("highscores");
 var startEL = document.getElementById("startnext");
@@ -32,7 +32,7 @@ var inEL2 = document.getElementById("initials2");
 var scEL3 = document.getElementById("score3");
 var inEL3 = document.getElementById("initials3");
 
-//============================================== Array with Quiz Questions Objects
+//============================================== Array with Quiz Questions Objects ==============================================
 const quizQuestions = [
   {
     //Question 1
@@ -81,6 +81,7 @@ const quizQuestions = [
   },
 ];
 //============================================== Start the game
+//start the game by resetting all counts, starting the timer and loading the questions plus jumping to game UI
 function startGame() {
   //set all counts to zero and reset timer
   questionCount = 0;
@@ -93,10 +94,9 @@ function startGame() {
   seeGame();
 }
 
-//============================================== Timer on the page
+//============================================== Timer on the page ==============================================
 //general timer function to create a timer starting at 75 counting down each second
 var timeLeft = 75;
-
 function countdown() {
   var timeInterval = setInterval(myTimer, 1000);
   function myTimer() {
@@ -121,6 +121,7 @@ function minusten() {
 }
 
 //timer color creates a sense of urgency through visual feedback
+//red for less than 20s, orange for less than 40s and more than 20s, green at the beginning
 function timerColor() {
   //indicate time left with color coding
   if (timeLeft >= 40) {
@@ -132,10 +133,11 @@ function timerColor() {
   }
 }
 
-//============================================== Get the questions from the array
+//============================================== Get the questions from the array ==============================================
 var questionCount = 0;
 var gameScore = 0;
 
+//populate questions
 function getQuestion() {
   if (questionCount < 5) {
     var getQuestionData = quizQuestions[questionCount];
@@ -153,8 +155,8 @@ function getQuestion() {
     setTimeout(showGameScore, 1000);
   }
 }
-//============================================== Evaluate Answers
-//evaluate answer
+//============================================== Evaluate Answers ==============================================
+//Check if given answer is correct
 function evaluateAnswer(event) {
   var givenAnswer = event.target.id;
   //check if the answer is the correct one, we have to deduct -1 since we increase the question count after loading the question
@@ -174,26 +176,26 @@ function evaluateAnswer(event) {
     getQuestion();
   }
 }
-
+//give feedback that the last answer was correct, delete after 1s
 function positiveFeedback() {
   console.log("correct");
   feedbackEL.textContent = "Correct! :-)";
   feedbackEL.style.color = "green";
   setTimeout(hideElement, 1000);
 }
-
+//give feedback that the last answer was wrong, delete after 1s
 function negativeFeedback() {
   console.log("wrong");
   feedbackEL.textContent = "Wrong! -10s :-/";
   feedbackEL.style.color = "red";
   setTimeout(hideElement, 1000);
 }
-
 function hideElement() {
   feedbackEL.textContent = "";
 }
-//============================================== Local Storage Handling and Ranking
-//give the user feedback about his/her ranking
+//============================================== Local Storage Handling and Ranking Feedback ==============================================
+//give the user feedback about his/her ranking of the recent game
+var rankResult = 0;
 function feedbackRanking() {
   saveBtn.textContent = "Save your score now";
   //get local storage
@@ -212,44 +214,53 @@ function feedbackRanking() {
   if (placeOne == null) {
     //new highscore
     rankEL.textContent = "yeah 1st place!";
+    rankResult = 1;
   } else if (placeTwo == null) {
     if (gameScore >= placeOne) {
       //new highscore
       rankEL.textContent = "yeah 1st place!";
+      rankResult = 1;
     } else {
       //second place
       rankEL.textContent = "Strong! 2nd place!";
+      rankResult = 2;
     }
   } else if (placeThree == null) {
     if (gameScore >= placeOne) {
       //new higscore
       rankEL.textContent = "yeah 1st place!";
+      rankResult = 1;
     } else if (gameScore >= placeTwo) {
       // second place
       rankEL.textContent = "Strong! 2nd place!";
+      rankResult = 2;
     } else {
       //third place
       rankEL.textContent = "Nice! You made it to the Olymp!";
+      rankResult = 3;
     }
   } else {
     if (gameScore >= placeOne) {
       //new higscore
       rankEL.textContent = "yeah 1st place!";
+      rankResult = 1;
     } else if (gameScore >= placeTwo) {
       // second place
       rankEL.textContent = "Strong! 2nd place!";
+      rankResult = 2;
     } else if (gameScore >= placeThree) {
       //third place
       rankEL.textContent = "Nice! You made it to the Olymp!";
+      rankResult = 3;
     } else {
       rankEL.textContent = "So close. No new highscore.";
       //make button to try again
       saveBtn.textContent = "Try again!";
+      rankResult = 4;
     }
   }
 }
-
-//higscore is rank 1
+//if the score is #1 on highscore list
 function newOne() {
   var initials = localStorage.getItem("latest");
   //#3 = old #2
@@ -266,12 +277,12 @@ function newOne() {
   localStorage.setItem("score1", gameScore);
   localStorage.setItem("initials1", initials);
 }
-//higscore is rank 2
+//if the score is #2 on highscore list
 function newTwo() {
   var initials = localStorage.getItem("latest");
   //#1 stays the same
   //#3 = old #2
-  if (localStorage.getItem("score1") != null) {
+  if (localStorage.getItem("score2") != null) {
     localStorage.setItem("score3", localStorage.getItem("score2"));
     localStorage.setItem("initials3", localStorage.getItem("initials2"));
   }
@@ -279,7 +290,7 @@ function newTwo() {
   localStorage.setItem("score2", gameScore);
   localStorage.setItem("initials2", initials);
 }
-//Highscore is rank 3
+//if the score is #3 on highscore list
 function newThree() {
   var initials = localStorage.getItem("latest");
   //#1 stays the same
@@ -288,69 +299,38 @@ function newThree() {
   localStorage.setItem("score3", gameScore);
   localStorage.setItem("initials3", initials);
 }
-
 //save the ranking
 function saveScore() {
+  //Exit if no new higscore
   if (saveBtn.textContent == "Try again!") {
     startGame();
     return;
   }
-
+  //ask  for initials to save
   var initials = prompt("Please type in your initials");
   localStorage.setItem("latest", initials);
-  //get local storage
-  var placeOne = localStorage.getItem("score1");
-  var placeTwo = localStorage.getItem("score2");
-  var placeThree = localStorage.getItem("score3");
 
-  //first loop: check how many local higscores are exisiting
-  //second loop within: check the rank within the existing higscore
-
-  if (placeOne == null) {
-    //new highscore
+  //check for ranking and save accordingly
+  if (rankResult == 1) {
+    console.log("execute1");
     newOne();
-  } else if (placeTwo == null) {
-    if (gameScore >= placeOne) {
-      //new highscore
-      newOne();
-    } else {
-      //second place
-      newTwo();
-    }
-  } else if (placeThree == null) {
-    if (gameScore >= placeOne) {
-      //new higscore
-      newOne();
-    } else if (gameScore >= placeTwo) {
-      // second place
-      newTwo();
-    } else {
-      //third place
-      newThree();
-    }
-  } else {
-    if (gameScore >= placeOne) {
-      //new higscore
-      newOne();
-    } else if (gameScore >= placeTwo) {
-      // second place
-      newTwo();
-    } else if (gameScore >= placeThree) {
-      //third place
-      newThree();
-    }
+  } else if (rankResult == 2) {
+    console.log("execute2");
+    newTwo();
+  } else if (rankResult == 3) {
+    console.log("execute3");
+    newThree();
   }
-  //jump to higscore page afterwards
+  //jump to updated highscore list
   seeHighScores();
 }
-
 //remove Local Storage
 function clearLocal() {
   localStorage.clear();
 
   seeHighScores();
 }
-//============================================== Switch between pages HighScores Page
+//============================================== Switch between pages ==============================================
 function seeHighScores() {
   quizA.setAttribute("class", "hidden");
   highA.setAttribute("class", "visible");
@@ -367,7 +347,6 @@ function seeHighScores() {
   scEL3.textContent = localStorage.getItem("score3");
   inEL3.textContent = localStorage.getItem("initials3");
 }
-
 function backToGame() {
   highA.setAttribute("class", "visible");
   quizA.setAttribute("class", "visible");
@@ -375,7 +354,6 @@ function backToGame() {
   questionA.setAttribute("class", "hidden");
   scoreA.setAttribute("class", "hidden");
 }
-
 function seeGame() {
   quizA.setAttribute("class", "hidden");
   highA.setAttribute("class", "hidden");
@@ -383,7 +361,6 @@ function seeGame() {
   scoreA.setAttribute("class", "hidden");
   highEL.setAttribute("class", "hidden");
 }
-
 function showGameScore() {
   quizA.setAttribute("class", "hidden");
   highA.setAttribute("class", "hidden");
@@ -396,11 +373,12 @@ function showGameScore() {
 
   feedbackRanking();
 }
-//============================================== Event Handler
+//============================================== Event Handler ==============================================
 //Navigation Items
 startEL.addEventListener("click", startGame);
 highEL.addEventListener("click", seeHighScores);
 backBtn.addEventListener("click", backToGame);
+//Buttons with actions attached
 saveBtn.addEventListener("click", saveScore);
 clearBtn.addEventListener("click", clearLocal);
 
